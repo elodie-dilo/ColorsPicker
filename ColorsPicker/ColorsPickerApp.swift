ac25d821ab2db1e5285ff2f50564824869064e8d
Skander BAHRI <skanderbahri@Skanders-MBP.lan>
2022-12-31 20:02:43 +0100

    first commit

---
+++ ColorsPicker/ColorsPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsPickerApp.swift
+//  ColorsPicker
+//
+//  Created by Skander BAHRI on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorsPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
